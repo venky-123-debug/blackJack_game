83c9771d6aa31d3c51c37437dd9c7d44cb8c1c0f
venkatesh c <venkat@print2block.com>
2024-03-28 16:28:58 +0530

    getting from deck based o random data has been completed

--- src/Pages/blackJack/index.svelte
+++ src/Pages/blackJack/index.svelte
@@ -17,7 +17,7 @@
   let randomNumber = 0
 
   let suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-  let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+  let values = ["A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
   onMount(() => {
     createDeck()
